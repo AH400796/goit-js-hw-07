3a8d3a795ef1951eaabcdbd8e50834977c4d629c
AH400796 <A.Hokhman@gmail.com>
2022-10-27 13:16:18 +0300

    1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,70 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+const markUpGallery = galleryItems
+  .map(({ preview, original, description }) => {
+    return `<div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
+    </a>
+  </div>`;
+  })
+  .join("");
+
+gallery.innerHTML = markUpGallery;
+
+gallery.addEventListener("click", clickOnImagePreview);
+
+let instance;
+let imagePreview;
+
+function clickOnImagePreview(event) {
+  event.preventDefault();
+
+  imagePreview = event.target;
+  instance = basicLightbox.create(`<img src="${event.target.dataset.source}">`);
+
+  showInstance();
+}
+
+function showInstance() {
+  if (imagePreview.nodeName !== "IMG") {
+    return;
+  } else {
+    instance.show();
+
+    addListenerOnDocument();
+    addListenerOnInstance();
+  }
+}
+
+function closeModalOnPressEscape(event) {
+  if (event.code !== "Escape") {
+    return;
+  } else {
+    instance.close();
+
+    removeListenerFromInstance();
+  }
+}
+
+function addListenerOnDocument() {
+  document.addEventListener("keydown", closeModalOnPressEscape);
+}
+
+function addListenerOnInstance() {
+  document
+    .querySelector(".basicLightbox")
+    .addEventListener("click", removeListenerFromInstance);
+}
+
+function removeListenerFromInstance() {
+  document.removeEventListener("keydown", closeModalOnPressEscape);
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,19 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+const markUpGallery = galleryItems
+  .map(({ preview, original, description }) => {
+    return `<a class="gallery__item" href="${original}">
+    <img class="gallery__image" src="${preview}" alt=${description}/>
+    </a>`;
+  })
+  .join("");
+
+gallery.innerHTML = markUpGallery;
+
+const ligthbox = new SimpleLightbox(".gallery a", {
+  captionType: "alt",
+  captionDelay: 250,
+});
